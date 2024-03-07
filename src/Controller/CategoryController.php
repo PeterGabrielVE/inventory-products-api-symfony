8092ca19f1dc2e2225cb35837896b8529207fec5
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-07 09:05:49 -0400

    servicios y controladors

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api', name: 'api_')]
+class CategoryController extends AbstractController
+{
+    #[Route('/categories', name: 'category_index', methods:['get'] )]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CategoryController.php',
+        ]);
+    }
+}
